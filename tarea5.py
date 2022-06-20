2b7aec118bad1aaf10e81d43a0ac89bda204e0ae
carlosaurelio94 <106714958+carlosaurelio94@users.noreply.github.com>
2022-06-20 19:15:59 -0300

    Add files via upload

---
+++ tarea5.py
@@ -0,0 +1,56 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+Nombre = "Pedro Pablo"
+Apellido = "Alvarez Perez"
+Edad = 25
+Email = "pedroalvarez@gmail.com"
+Telefono = 1124524785
+Casado = True
+conHijos = False
+listaDeAmigos = ['Maria', 'Jose', 'Andrea', 'Francisco']
+peliculasVistas = {
+    'clave': 'Los vengadores';
+    
+SyntaxError: '{' was never closed
+peliculasVistas = {
+    'clave': 'Los vengadores';
+    
+SyntaxError: '{' was never closed
+peliculasVistas = {
+    'clave': 'Los vengadores',
+    'clave2': 'Rapido y furioso'
+    }
+    
+print(Nombre)
+    
+Pedro Pablo
+print (Nombre, Apellido)
+    
+Pedro Pablo Alvarez Perez
+Print(Edad)
+    
+Traceback (most recent call last):
+  File "<pyshell#18>", line 1, in <module>
+    Print(Edad)
+NameError: name 'Print' is not defined. Did you mean: 'print'?
+print(Edad)
+    
+25
+print(Email)
+    
+pedroalvarez@gmail.com
+print(Telefono)
+    
+1124524785
+print(Casado)
+    
+True
+print(conHijos)
+    
+False
+print(listaDeAmigos)
+    
+['Maria', 'Jose', 'Andrea', 'Francisco']
+print(peliculasVistas)
+    
+{'clave': 'Los vengadores', 'clave2': 'Rapido y furioso'}
